c8fa3218c165e58cd6556236200b853b77b15b51
ABOUSSALIM <aboussalimoussama7@gmail.com>
2024-12-10 22:05:39 +0100

    my commit graphQL

--- src/main/java/ma/projet/graph/controllers/CompteControllerGraphQL.java
+++ src/main/java/ma/projet/graph/controllers/CompteControllerGraphQL.java
@@ -1,8 +1,13 @@
 package ma.projet.graph.controllers;
 
 import lombok.AllArgsConstructor;
+import ma.projet.graph.dto.TransactionRequest;
 import ma.projet.graph.entities.Compte;
+import ma.projet.graph.entities.Transaction;
+import ma.projet.graph.entities.TypeCompte;
+import ma.projet.graph.entities.TypeTransaction;
 import ma.projet.graph.repositories.CompteRepository;
+import ma.projet.graph.repositories.TransactionRepository;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -17,6 +22,8 @@ public class CompteControllerGraphQL {
 
     private CompteRepository compteRepository;
 
+    private TransactionRepository transactionRepository;
+
     @QueryMapping
     public List<Compte> allComptes(){
         return compteRepository.findAll();
@@ -27,6 +34,16 @@ public class CompteControllerGraphQL {
         Compte compte =  compteRepository.findById(id).orElse(null);
         if(compte == null) throw new RuntimeException(String.format("Compte %s not found", id));
         else return compte;
+    }
+
+    @QueryMapping
+    public List<Compte> compteByType(@Argument TypeCompte type) {
+        return compteRepository.findByType(type);
+    }
+
+    @MutationMapping
+    public void deleteCompte(@Argument Long id){
+        compteRepository.deleteById(id);
     }
 
     @MutationMapping
@@ -46,4 +63,42 @@ public class CompteControllerGraphQL {
                 "average", average
         );
     }
+
+    @MutationMapping
+    public Transaction addTransaction(@Argument TransactionRequest transactionRequest) {
+
+        System.out.println(transactionRequest.getCompteId()+"###################################");
+        Compte compte = compteRepository.findById(transactionRequest.getCompteId())
+                .orElseThrow(() -> new RuntimeException("Compte not found"));
+
+        Transaction transaction = new Transaction();
+        transaction.setMontant(transactionRequest.getMontant());
+        transaction.setDateTransaction(transactionRequest.getDateTransaction());
+        transaction.setType(transactionRequest.getType());
+        transaction.setCompte(compte);
+        transactionRepository.save(transaction);
+        return transaction;
+    }
+
+
+    @QueryMapping
+    public List<Transaction> compteTransactions(@Argument Long id) {
+        Compte compte = compteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Compte not found"));
+        return transactionRepository.findByCompte(compte);
+    }
+
+
+    @QueryMapping
+    public Map<String, Object> transactionStats() {
+        long count = transactionRepository.count();
+        double sumDepots = transactionRepository.sumByType(TypeTransaction.DEPOT);
+        double sumRetraits = transactionRepository.sumByType(TypeTransaction.RETRAIT);
+        return Map.of(
+                "count", count,
+                "sumDepots", sumDepots,
+                "sumRetraits", sumRetraits
+        );
+    }
+
 }
